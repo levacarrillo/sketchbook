df6167dc79c7cb69f8861a6823f209bd1a276bc1
Luis Nava <levacarrillo@gmail.com>
2020-08-26 15:00:44 -0500

    Added  into Makefile for being more robust

--- sensors_test/sensors_test.ino
+++ sensors_test/sensors_test.ino
@@ -10,17 +10,19 @@ void loop() {
 	
 	read_sensors_data();
 	
+	Serial.print("[");
 	Serial.print(left_sharp());
-	Serial.print("--");
+	Serial.print(", ");
 	Serial.print(right_sharp());
-	Serial.print("--");
+	Serial.print(", ");
 	Serial.print(ldr0());
-	Serial.print("--");
+	Serial.print(", ");
 	Serial.print(ldr1());
-	Serial.print("--");
+	Serial.print(", ");
 	Serial.print(ldr2());
-	Serial.print("--");
-	Serial.println(ldr3());
+	Serial.print(", ");
+	Serial.print(ldr3());
+	Serial.println("]");	
 	
 	delay(200);	
 }
