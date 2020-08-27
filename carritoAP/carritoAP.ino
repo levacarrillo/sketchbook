c9247cbb07f095b827a0df13eff06c6ebc2cd3f6
rmartella <reymar_44@hotmail.com>
2020-08-26 20:32:04 -0500

    carritoAP.ino improved ros.h modified buffer size for arduino UNO

--- carritoAP/carritoAP.ino
+++ carritoAP/carritoAP.ino
@@ -10,6 +10,11 @@
 
 ros::NodeHandle nh;
 
+float goal_speed[2];
+int sharp_sensor[2];
+int light_sensor[4];
+long encoder_data[2];
+
 std_msgs::Int32MultiArray encoders_msg;
 std_msgs::Int16MultiArray light_sensors_msgs;
 std_msgs::Int16MultiArray sharp_sensors_msgs;
@@ -21,35 +26,45 @@ ros::Publisher lightSensorsPub("/light_sensors", &light_sensors_msgs);
 ros::Publisher sharpSensorsPub("/sharp_sensors", &sharp_sensors_msgs);
 ros::Subscriber<std_msgs::Float32MultiArray> subSpeedMotors("/speed_motors", speedMotorsCallback);
 
+
+
+void publish_odom() {
+
+	encoders_msg.data_length = 2;
+	encoder_data[0] =   left_count();
+	encoder_data[1] =  right_count();
+	encoders_msg.data = encoder_data;
+
+	odomPub.publish(&encoders_msg);
+}
+
+
 void publish_sensors_data() {
 	
 	read_sensors_data();
 	sharp_sensors_msgs.data_length = 2;
 	light_sensors_msgs.data_length = 4;
 
-	light_sensors_msgs.data[0] = ldr0();
-	light_sensors_msgs.data[1] = ldr1();
-	light_sensors_msgs.data[2] = ldr2();
-	light_sensors_msgs.data[3] = ldr3();
-	sharp_sensors_msgs.data[0] =  left_sharp();
-	sharp_sensors_msgs.data[1] = right_sharp();
+	light_sensor[0] = ldr0();
+	light_sensor[1] = ldr1();
+	light_sensor[2] = ldr2();
+	light_sensor[3] = ldr3();
+	sharp_sensor[0] =  left_sharp();
+	sharp_sensor[1] = right_sharp();
+	
+	light_sensors_msgs.data = light_sensor;
+	sharp_sensors_msgs.data = sharp_sensor;
 
 	lightSensorsPub.publish(&light_sensors_msgs);
 	sharpSensorsPub.publish(&sharp_sensors_msgs);
 }
 
-void publish_odom() {
 
-	encoders_msg.data_length = 2;
-	encoders_msg.data[left_count()];
-	encoders_msg.data[right_count()];
-
-	odomPub.publish(&encoders_msg);
-}
 	
 void speedMotorsCallback(const std_msgs::Float32MultiArray& msg) {
 	
-	motors_speed_pid(msg.data[0], msg.data[1]);
+	goal_speed[0] = msg.data[0];
+	goal_speed[1] = msg.data[1];
 }
 
 
@@ -61,6 +76,7 @@ void setup() {
 	nh.advertise(odomPub);
 	nh.advertise(lightSensorsPub);
 	nh.advertise(sharpSensorsPub);
+	nh.subscribe(subSpeedMotors);
 
 	set_encoders_input();
 	set_pid_parameters();
@@ -71,6 +87,7 @@ void loop() {
 	
 	publish_odom();
 	publish_sensors_data();
+	motors_speed_pid(goal_speed[0], goal_speed[1]);
 
 	nh.spinOnce();
 	delay(20);
--- libraries/ros_lib/ros.h
+++ libraries/ros_lib/ros.h
@@ -51,7 +51,7 @@ namespace ros
 
 #elif defined(__AVR_ATmega328P__)
 
-  typedef NodeHandle_<ArduinoHardware, 25, 25, 280, 280> NodeHandle;
+  typedef NodeHandle_<ArduinoHardware, 5, 5, 150, 150> NodeHandle; // default-> 25 25 280 280
 
 #elif defined(SPARK)
 
